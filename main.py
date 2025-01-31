de3c9d76e4d62ccf221bfad16f834b9c5f6aa146
kanchang12 <32091338+kanchang12@users.noreply.github.com>
2025-01-31 16:25:18 +0000

    Update main.py

--- main.py
+++ main.py
@@ -20,4 +20,4 @@ def place_call():
         return jsonify({"message": "Error placing the call"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=8000)
